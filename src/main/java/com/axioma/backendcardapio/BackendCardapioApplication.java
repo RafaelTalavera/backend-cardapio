e5fc86f2f5d1262d033437f8b72a09010df3deae
RafaelTalavera <rtalavera@live.com.ar>
2023-11-16 16:48:43 -0300

    firts commit include dependencies pom.xls

---
+++ src/main/java/com/axioma/backendcardapio/BackendCardapioApplication.java
@@ -0,0 +1,13 @@
+package com.axioma.backendcardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCardapioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCardapioApplication.class, args);
+    }
+
+}
